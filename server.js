f428eb79121826470e46108a411ef39f3d8ef641
deleav <a42006310@gmail.com>
2016-09-07 01:31:57 +0800

    let .dev.html can run on express server

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ var server = new WebpackDevServer(compiler, {
   hot: true,
   historyApiFallback: true,
   stats: { colors: true },
+  setup: function(app) {
+    app.engine('dev.html', require('ejs').renderFile);
+    app.set('views', './');
+    app.set('view engine', 'dev.html');
+    app.get('/', function(req, res) {
+      res.render('index');
+    });
+  },
 }).listen(8080, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
